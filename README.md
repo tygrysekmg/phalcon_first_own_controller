d559830eeaf26bf1dccaf04429db0aed6cc00bb8
root <root@grzegorz-php72.com>
2018-03-14 13:24:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# phalcon_first_own_controller
+follow for images p1.png p2.png p2.png
---
+++ app/controllers/PiotrController.php
@@ -0,0 +1,14 @@
+<?php
+
+class PiotrController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+    
+      echo "This is my first web application in Phalcon"; 
+    
+    }
+
+}
+
